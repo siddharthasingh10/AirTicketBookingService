ad900bee22027e3c35859bec9ee71ad467b7ceba
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2024-10-06 05:33:03 +0530

    create booking functon done

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,21 @@
+const {Booking}=require('./../models/index');
+const {ValidationError, AppError}=require('../utils/errors/index');
+const {StatusCodes}=require('http-status-codes')
+class BookingRepository{
+    async create(data){
+        try{
+            const booking =await Booking.create(data);
+            return booking;
+        }
+        catch(error){
+            if(error.name=='SequelizeValidationError'){
+                throw new ValidationError(error);
+            }
+            throw new AppError(
+                'RepositoryError','Cannot create a booking','There is some issue creating booking ',StatusCodes.INTERNAL_SERVER_ERROR
+            )
+        }
+    }
+
+}
+module.exports=BookingRepository;
---
+++ src/utils/errors/index.js
@@ -0,0 +1,7 @@
+
+
+module.exports={
+    ValidationError:require('./validation-error'),
+    AppError:require('./validation-error'),
+    ServiceError:require('./validation-error')
+}
